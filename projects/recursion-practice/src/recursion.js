d374e0fb9dac3577d3788c771f056ad0dea8d061
MP4swerve <key.investnola@gmail.com>
2024-02-28 13:04:22 -0600

    gyuuh

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -32,7 +32,14 @@ var sum = function(array, num = 0) {
 
 // 3. Sum all numbers in an array containing nested arrays.
 // Example: arraySum([1,[2,3],[[4]],5]); // 15
-var arraySum = function(array) {
+var arraySum = function(array, output = 0) {
+  // base
+  if(array.length === 0){
+    return output;
+  }
+  // recursion
+  output += array[0];
+  return arraySum(array.slice(1), output)
 };
 
 // 4. Check if a number is even.
