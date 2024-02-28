3a937f2a23a67d0b6800e107f9b76bab292b33ca
MP4swerve <key.investnola@gmail.com>
2024-02-28 12:59:02 -0600

    rujdnfbv

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -20,7 +20,14 @@ var factorial = function(n, product = 1) {
 
 // 2. Compute the sum of an array of integers.
 // Example:  sum([1, 2, 3, 4, 5, 6]);  // 21
-var sum = function(array) {
+var sum = function(array, num = 0) {
+  // base
+  if(array.length === 0){
+    return num;
+  }
+  // recursion
+  num += array[0];
+  return sum(array.slice(1), num)
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
