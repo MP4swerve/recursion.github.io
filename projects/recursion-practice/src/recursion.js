097c905362994ebcb805825af72b30ba20bc94e8
MP4swerve <key.investnola@gmail.com>
2024-03-13 15:21:30 -0500

    sds

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -430,7 +430,7 @@ return alternateSign(array, index + 1);
 // 35. Given a string, return a string with digits converted to their word equivalent.
 // Assume all numbers are single digits (less than 10).
 // numToText("I have 5 dogs and 6 ponies"); // "I have five dogs and six ponies"
-var numToText = function(str) {
+var numToText = function(str, index = 0, result = '') {
   //base
   if (index === str.length) {
     return result;
